f01c7004df2c95e703f4fe4e9fc2fcf3b6f34497
yumin-lab <n12345@snu.ac.kr>
2016-07-21 20:20:57 +0900

    .

--- datasets/mpii-gen.lua
+++ datasets/mpii-gen.lua
@@ -1,7 +1,32 @@
 local M = {}
 
+function findImages()
+  local imagePath = torch.CharTensor()
+  
+end
+
 function M.exec(opt, cacheFile)
-   paths.concat(opt.datasetDir, dataset)
+  local trainDir = paths.concat(opt.datasetDir,) 
+  local valDir = paths.concat(opts.datasetDir,)
+  
+  local valImagePaths, valLabels = findImages(valDir)
+  local trainImagePaths, trainLabels = findImages(trainDir)
+    
+  local info = {
+      basedir = opt.datasetDir,
+      train = {
+        imagePaths = trainImagePaths,
+        labels = trainLabels,
+      },
+      val = {
+        imagePaths = valImagePaths,
+        labels = valLabels,
+      },
+    }
+  
+  print("Saving list of images to " .. cacheFile)
+  torch.save(cacheFile, info)
+  return info
 end
 
 return M
