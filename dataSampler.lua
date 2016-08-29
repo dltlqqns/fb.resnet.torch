21d6b2ccf90005a9e4bda99d9c2b178d8afb8dde
yumin-lab <n12345@snu.ac.kr>
2016-08-29 21:57:44 +0900

    .

---
+++ dataSampler.lua
@@ -0,0 +1,14 @@
+local M = {}
+local DataSampler = torch.class('DataSampler', M)
+
+function DataSampler:__init(anchors)
+end
+
+function DataSampler:samplePositives()
+  
+end
+
+function DataSampler:sampleNegatives()
+end
+
+return M.DataSampler
--- trainBaseline1.lua
+++ trainBaseline1.lua
@@ -1,3 +1,4 @@
+--[[
 local M = {}
 local Trainer = torch.class('Trainer', M)
 
@@ -6,10 +7,29 @@ function Trainer:__init(model, criterion, opt, optimState)
   self.
 end
 
-function Trainer:train()
+function Trainer:train(epoch, dataloader)
+  
+  for n, sample in dataloader:run() do
+    -- Copy samples to GPU
+    self:copySample(sample)
+    
+    -- Foward / backward
+    local outputs = self.model:forward(self.inputs)
+    local loss = self.criterion:forward(self.model.output, self.target)
+    
+    self.model:zeroGradParameters()
+    self.criterion:backward(self.model.output, self.target)
+    self.model:backward
+    
+    -- Optimize
+    
+    --
+  end
+  
 end
 
 function Trainer:test()
 end
 
 return M.Trainer
+--]]
